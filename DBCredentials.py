ae9b9c2ee4b335d93b0b38301d54f25903ab34c2
root <root@TuxML.istic.univ-rennes1.fr>
2018-04-04 17:19:32 +0200

    Fix dev db used instead of prod

--- DBCredentials.py
+++ DBCredentials.py
@@ -13,7 +13,7 @@ irmaDB_v2 = {
         "user":"csvgen",
         "passwd":"yHGE0Km4bxxKoYdb",
         "host":"148.60.11.195",
-        "db": "IrmaDB_dev",
+        "db": "IrmaDB_prod",
     },
     "bz2":True,
     "table":"Compilations"
