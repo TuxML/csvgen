66068f145fcbd9fccb7639a8c6f6fc17f7681185
root <root@TuxML.istic.univ-rennes1.fr>
2018-04-04 16:51:13 +0200

    Fix naming

--- genCSV_irmaDB.sh
+++ genCSV_irmaDB.sh
@@ -6,9 +6,9 @@ if [ -s "$1" ] ; then
 fi
 
 echo "V1 : "
-python3 "./genCSV_v1.py" "${1}_v1.tmp"
+python3 "./genCSV_irmaDB_v1.py" "${1}_v1.tmp"
 echo "V2 : "
-python3 "./genCSV_v2.py" "${1}_v2.tmp"
+python3 "./genCSV_irmaDB_v2.py" "${1}_v2.tmp"
 
 echo "Merging..."
 ./merge.sh "${1}_v1.tmp" "${1}_v2.tmp" "$1"
